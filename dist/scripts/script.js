9987765f73e9d359f56ec3d11d952f9541db5e25
Ruslan <vjik.jig@gmail.com>
2019-10-03 23:43:44 +0300

    added server, changed file sctructure

--- dist/scripts/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var hello = function hello(name) {
-  return "hello ".concat(name);
-};
-
-var test = "test";
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,28 @@ const gulp = require("gulp");
 const gulpSass = require("gulp-sass");
 const removeFiles = require("gulp-remove-files");
 const babel = require("gulp-babel");
+const connect = require("gulp-connect");
+
+gulp.task("serve", done => {
+  connect.server({
+    root: "./",
+    port: 8001,
+    livereload: true
+  });
+  done();
+});
+
+gulp.task("html", done => {
+  gulp.src("./*.html").pipe(connect.reload());
+  done();
+});
 
 gulp.task("scripts", done => {
   gulp
     .src("./js/script.js")
     .pipe(babel({ presets: ["@babel/preset-env"] }))
-    .pipe(gulp.dest("./dist/scripts/"));
+    .pipe(gulp.dest("./dist/scripts/"))
+    .pipe(connect.reload());
   done();
 });
 
@@ -20,13 +36,18 @@ gulp.task("styles", done => {
   gulp
     .src("./styles/main/main.scss")
     .pipe(gulpSass())
-    .pipe(gulp.dest("./dist/styles/"));
+    .pipe(gulp.dest("./dist/styles/"))
+    .pipe(connect.reload());
   done();
 });
 
 gulp.task("watch", () => {
   gulp.watch("./styles/main/main.scss", gulp.series("styles"));
   gulp.watch("./js/script.js", gulp.series("scripts"));
+  gulp.watch("./*.html", gulp.series("html"));
 });
 
-gulp.task("default", gulp.series("clear", "scripts", "styles", "watch"));
+gulp.task(
+  "default",
+  gulp.series("clear", "scripts", "styles", "serve", "watch")
+);
